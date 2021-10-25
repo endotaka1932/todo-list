24416725e4191661d6c927c317a152c3db96c6df
遠藤貴文 <>
2021-10-25 22:53:42 +0900

    board一覧ページの作成（indexアクション）

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,39 @@
+class BoardsController < ApplicationController
+    before_action :set_board, only: [:show]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+    def index
+        @boards = Board.all
+    end
+
+    def show
+    end
+
+    def new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save!
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def destroy
+    end
+
+    private
+    def board_params
+      puts '---------------'
+      puts params
+      puts '---------------'
+      params.require(:board).permit(:title, :content)
+    end
+
+    def set_board
+      @board = Board.find(params[:id])
+    end
+end
