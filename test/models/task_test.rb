d88335d1e797383212b74d5c3c28baa0cd40324f
遠藤貴文 <>
2021-10-28 22:58:58 +0900

    taskモデル作成 他モデルとの紐付け済

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,20 +1,23 @@
 # == Schema Information
 #
-# Table name: boards
+# Table name: tasks
 #
 #  id         :bigint           not null, primary key
 #  content    :text             not null
+#  deadline   :integer
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint           not null
+#  board_id   :bigint           not null
 #
 # Indexes
 #
-#  index_boards_on_user_id  (user_id)
+#  index_tasks_on_board_id  (board_id)
 #
-class Board < ApplicationRecord
-    belongs_to :User, optional: true
-    has_many :tasks, dependent: :destroy
+require 'test_helper'
 
+class TaskTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
 end
